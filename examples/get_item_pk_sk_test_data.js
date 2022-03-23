d5844b6e6094ebbf0b4d6f053878e65b174a487d
Martin <martin.mphil@gmail.com>
2022-03-23 08:00:52 +0000

    AWS lambda-2.0-format response

--- examples/get_item_pk_sk_test_data.js
+++ examples/get_item_pk_sk_test_data.js
@@ -1,35 +1,21 @@
 const AWS = require("aws-sdk");
-
 const dynamo = new AWS.DynamoDB.DocumentClient();
+const TableName = "pine";
 
 exports.handler = async (event, context) => {
-  let body;
-  let statusCode = 200;
-  const headers = {
-    "Content-Type": "application/json",
-  };
-
   try {
     const dbData = await dynamo
       .get({
-        TableName: "pine",
+        TableName,
         Key: {
           pk: "text1",
           sk: "4Multichoice",
         },
       })
       .promise();
-    body = dbData?.Item?.markup;
+    markup = dbData?.Item?.markup;
+    return { body: JSON.stringify(markup) };
   } catch (err) {
-    statusCode = 400;
-    body = err.message;
-  } finally {
-    body = JSON.stringify(body);
+    return { error: err };
   }
-
-  return {
-    statusCode,
-    body,
-    headers,
-  };
 };
