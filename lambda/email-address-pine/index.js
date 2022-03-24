fb76ed18351a9caf9ed236d76a600ea379d53fa8
Martin <martin.mphil@gmail.com>
2022-03-24 07:53:15 +0000

    return {body} or {error} like db query lambdas

--- lambda/email-address-pine/index.js
+++ lambda/email-address-pine/index.js
@@ -1,18 +1,17 @@
 const AWS = require("aws-sdk");
 
 exports.handler = async function (event) {
-  const AccessToken = event.headers.authorization;
+  try {
+    const AccessToken = event.headers.authorization;
 
-  const cog = new AWS.CognitoIdentityServiceProvider({
-    apiVersion: "2016-04-18",
-  });
-  const cogUser = await cog.getUser({ AccessToken }).promise();
-  const email = cogUser.UserAttributes.find((x) => x.Name === "email").Value;
+    const cog = new AWS.CognitoIdentityServiceProvider({
+      apiVersion: "2016-04-18",
+    });
+    const cogUser = await cog.getUser({ AccessToken }).promise();
+    const email = cogUser.UserAttributes.find((x) => x.Name === "email").Value;
 
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify(email),
-  };
-  return response;
+    return { body: JSON.stringify(email) };
+  } catch (error) {
+    return { error };
+  }
 };
-
