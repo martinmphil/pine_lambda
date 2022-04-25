f14ce37827a1ad9bb59f540ac9998c026a3cc784
Martin <martin.mphil@gmail.com>
2022-04-25 07:49:26 +0100

    general lambda coding pattern

--- examples/proforma_db_calls.js
+++ examples/proforma_db_calls.js
@@ -2,28 +2,38 @@ const AWS = require("aws-sdk");
 const dynamo = new AWS.DynamoDB.DocumentClient();
 const TableName = "pine";
 
-exports.handler = async (event, context) => {
+const dbPromise = (pk, sk) => {
+  return dynamo
+    .get({
+      TableName,
+      Key: {
+        pk,
+        sk,
+      },
+    })
+    .promise()
+    .then((dbItem) => {
+      return dbItem?.Item?.arr;
+    })
+    .catch((err) => {
+      const fault = ` Get db-data for ${pk} failed:- ${JSON.stringify(err)} `;
+      console.warn(fault);
+      throw fault;
+    });
+};
+
+exports.handler = async (event) => {
   try {
     const username = event.requestContext.authorizer.jwt.claims.username;
-
     const pk = `candidate#${username}`;
-
     const sk = "ongoing";
-
-    const dbData = await dynamo
-      .get({
-        TableName,
-        Key: {
-          pk,
-          sk,
-        },
-      })
-      .promise()
-      .then((dbItem) => {
-        return dbItem?.Item?.arr;
-      });
+    const dbData = await dbPromise(pk, sk);
     return { body: JSON.stringify(dbData) };
   } catch (err) {
-    return { error: err };
+    const fault = ` Lambda my_fn_name failed:- ${JSON.stringify(err)} `;
+    console.warn(fault);
+    return {
+      error: fault,
+    };
   }
 };
