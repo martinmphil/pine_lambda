0d973047f5d2dc8ff6ac060b8cbc4466a45b51f8
Martin <martin.mphil@gmail.com>
2022-04-28 11:21:05 +0100

    pattern to throw error for invalid db response

--- examples/proforma_db_calls.js
+++ examples/proforma_db_calls.js
@@ -2,7 +2,7 @@ const AWS = require("aws-sdk");
 const dynamo = new AWS.DynamoDB.DocumentClient();
 const TableName = "pine";
 
-const dbPromise = (pk, sk) => {
+async function dbGet(pk, sk) {
   return dynamo
     .get({
       TableName,
@@ -13,21 +13,20 @@ const dbPromise = (pk, sk) => {
     })
     .promise()
     .then((dbItem) => {
-      return dbItem?.Item?.arr;
-    })
-    .catch((err) => {
-      const fault = ` Get db-data for ${pk} failed:- ${JSON.stringify(err)} `;
-      console.warn(fault);
-      throw fault;
+      const result = dbItem?.Item?.obj;
+      if (result === undefined || null) {
+        throw ` DynamoDB get (${pk}, ${sk}) failed. `;
+      }
+      return result;
     });
-};
+}
 
 exports.handler = async (event) => {
   try {
     const username = event.requestContext.authorizer.jwt.claims.username;
     const pk = `candidate#${username}`;
     const sk = "ongoing";
-    const dbData = await dbPromise(pk, sk);
+    const dbData = await dbGet(pk, sk);
     return { body: JSON.stringify(dbData) };
   } catch (err) {
     const fault = ` Lambda my_fn_name failed:- ${JSON.stringify(err)} `;
