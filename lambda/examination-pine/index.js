f0938474292897af12faa259422be99d872ea208
Martin <martin.mphil@gmail.com>
2022-05-02 08:01:10 +0100

    naming consistent with server URI conventions

--- lambda/examination-pine/index.js
+++ lambda/examination-pine/index.js
@@ -2,16 +2,18 @@ exports.handler = async (event) => {
   try {
     //const username = event.requestContext.authorizer.jwt.claims.username;
     const username = "dummy_user";
-    console.log(event + " " + username);
+    console.log(JSON.stringify(event) + " " + username);
     //
     //
     const canId = `candidate#${username}`;
 
-    const nextQHtml = canId;
+    const idNbr = event.pathParameters.idNbr;
+
+    const nextQHtml = `user ${canId} has exam idNbr of ${idNbr}`;
 
     return { body: JSON.stringify(nextQHtml) };
   } catch (err) {
-    const fault = ` Lambda fn can-listings-pine failed:- ${JSON.stringify(
+    const fault = ` Lambda fn examination-pine failed:- ${JSON.stringify(
       err
     )} `;
     return { error: fault };
