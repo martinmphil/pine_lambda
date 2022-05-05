08d5e0b99cfdd95a41103ebd9f69842149c2a705
Martin <martin.mphil@gmail.com>
2022-05-05 14:19:58 +0100

    examination process fn

--- lambda/examination-pine/index.js
+++ lambda/examination-pine/index.js
@@ -1,16 +1,39 @@
 const AWS = require("aws-sdk");
 const dynamo = new AWS.DynamoDB.DocumentClient();
 const TableName = "pine";
-// const timestamp = new Date().toISOString();
+const timestamp = new Date().toISOString();
 let fault = "";
+let qList = [];
+let qIndex = 0;
 
-async function getAssessmentData(canId, examId) {
+async function examinationProcess(canId, examId) {
+  const assessmentData = await getAssessmentData(canId, examId);
+
+  if (assessmentData === undefined || null) {
+    qIndex = 0;
+    qList = await getQList(examId);
+
+    if (qList === undefined || qList.length === 0) {
+      throw ` Question-list missing for exam ${examId} `;
+    }
+
+    await putAssessmentData(canId, examId, qIndex);
+
+    //
+    //
+    // To complete the top line of the exam flow chart I need to create submissions record for qList[qIndex]
+  }
+
+  return qList;
+}
+
+async function getDbItem(pk, sk) {
   return dynamo
     .get({
       TableName,
       Key: {
-        pk: canId,
-        sk: examId,
+        pk,
+        sk,
       },
     })
     .promise()
@@ -19,22 +42,57 @@ async function getAssessmentData(canId, examId) {
     });
 }
 
+async function getAssessmentData(canId, examId) {
+  return getDbItem(canId, examId);
+}
+
+async function getQList(examId) {
+  return getDbItem(examId, examId).then((Item) => {
+    return Item?.qList;
+  });
+}
+
+async function putAssessmentData(canId, examId, qIndex) {
+  dynamo
+    .put({
+      TableName,
+      Item: {
+        pk: canId,
+        sk: examId,
+        entityType: "assessmentData",
+        qIndex,
+        qList,
+        startTime: timestamp,
+      },
+      ConditionExpression:
+        "attribute_not_exists(pk) AND attribute_not_exists(sk)",
+    })
+    .promise()
+    .catch((err) => {
+      const fault = ` put-assessment-data failed for user ${canId} and exam ${examId}:- ${JSON.stringify(
+        err
+      )} `;
+      console.warn(fault);
+      throw fault;
+    });
+}
+
 exports.handler = async (event) => {
   try {
     //const username = event.requestContext.authorizer.jwt.claims.username;
+    // const examCode = event.pathParameters.idNbr;
+    const examCode = "1";
     const username = "dummy_user";
     console.log(JSON.stringify(event) + " " + username);
     //
-    //
     const canId = `candidate#${username}`;
-    const examCode = event.pathParameters.idNbr;
-    const examId = `èxam#${examCode}`;
+    const examId = `exam#${examCode}`;
 
-    const assessmentRecord = await getAssessmentData(canId, examId);
+    const bravo = await examinationProcess(canId, examId);
 
     // html output
     const nextQHtml = `user is ${canId} and exam is ${examId} and assessment record is ${JSON.stringify(
-      assessmentRecord
+      bravo
     )}`;
 
     return { body: JSON.stringify(nextQHtml) };
