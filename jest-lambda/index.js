4b328a14848a41a537c8c8253cb82729d98cff5e
Martin <martin.mphil@gmail.com>
2022-05-12 12:17:20 +0100

    index scaffold

--- jest-lambda/index.js
+++ jest-lambda/index.js
@@ -1,7 +1,27 @@
-const AWS = require("aws-sdk");
+// const AWS = require("aws-sdk");
 const getDbItemMod = require("./getDbItemMod");
+const timestamp = new Date().toISOString();
+let fault = "";
+// let qList = [];
+// let qIndex = 0;
 
-exports.handler = async () => {
-  const result = getDbItemMod.getDbItem;
-  return { body: JSON.stringify(result) };
+exports.handler = async (event) => {
+  try {
+    // const username = event.requestContext.authorizer.jwt.claims.username;
+    // const examCode = event.pathParameters.idNbr;
+    const examCode = "1";
+    const username = "dummy_user";
+    // to remove
+    console.log(JSON.stringify(event) + " " + username);
+
+    const canId = `candidate#${username}`;
+    const examId = `exam#${examCode}`;
+
+    const nextQHtml = `user is ${canId} and exam is ${examId}. `;
+
+    return { body: JSON.stringify(nextQHtml) };
+  } catch (err) {
+    fault += ` Lambda fn examination-pine failed:- ${JSON.stringify(err)} `;
+    return { error: fault };
+  }
 };
