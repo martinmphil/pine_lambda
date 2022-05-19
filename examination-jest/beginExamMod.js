eefbd645bde57ab650dc96ee39381d7ce1e4c88a
Martin <martin.mphil@gmail.com>
2022-05-19 14:25:21 +0100

    test output returns both candidate-id and q-list

--- examination-jest/beginExamMod.js
+++ examination-jest/beginExamMod.js
@@ -2,7 +2,7 @@ const { getQList } = require("./getQListMod");
 
 async function beginExam(canId, examId) {
   const qList = await getQList(examId);
-  return `this is beginExam return ${JSON.stringify(qList)}`;
+  return `this is beginExam for ${canId} q-list ${JSON.stringify(qList)}`;
 }
 
 exports.beginExam = beginExam;
