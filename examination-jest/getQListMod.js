1e9015828dfa756f6e28a2dca92b6611336e9d99
Martin <martin.mphil@gmail.com>
2022-05-24 07:57:45 +0100

    validate question-list after db get request

--- examination-jest/getQListMod.js
+++ examination-jest/getQListMod.js
@@ -1,9 +1,19 @@
 const { getDbItem } = require("./getDbItemMod");
 
+const validStr = (x) => typeof x === "string" && x.length > 0;
+const checkProps = (y) => validStr(y.pKey) && validStr(y.sKey);
+const validQList = (z) => z.every(checkProps);
+
 async function getQList(examId) {
-  return getDbItem(examId, examId).then((Item) => {
+  const qList = await getDbItem(examId, examId).then((Item) => {
     return Item?.qList;
   });
+  if (validQList(qList)) {
+    return qList;
+  } else throw ` Invalid question-list for exam ${examId}. `;
 }
 
 exports.getQList = getQList;
+
+// for unit test
+exports.validQList = validQList;
