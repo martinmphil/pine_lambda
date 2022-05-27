385f46578bf9584d958bf59ccb20751a36c74d71
Martin <martin.mphil@gmail.com>
2022-05-27 07:11:26 +0100

    module initialising submission record in db

---
+++ examination-jest/initSubmissionMod.js
@@ -0,0 +1,23 @@
+const { putDbItem } = require("./putDbItemMod");
+
+const timestamp = new Date().toISOString();
+
+async function initSubmission(canId, qTextId) {
+  const putParamObj = {
+    TableName: "pine",
+    Item: {
+      pk: canId,
+      sk: qTextId,
+      entityType: "submissionData",
+      createdAt: timestamp,
+    },
+    ConditionExpression:
+      "attribute_not_exists(pk) AND attribute_not_exists(sk)",
+  };
+  return putDbItem(putParamObj);
+}
+
+exports.initSubmission = initSubmission;
+
+// for unit test
+exports.putDbItem = putDbItem;
