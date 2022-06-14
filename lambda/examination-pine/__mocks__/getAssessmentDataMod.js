770377bb46e4289e228f9c564264deae023780ba
Martin <martin.mphil@gmail.com>
2022-06-14 08:15:41 +0100

    mock return values once for module reuse

--- lambda/examination-pine/__mocks__/getAssessmentDataMod.js
+++
@@ -1,7 +0,0 @@
-const { dummyAssessmentData } = require("./dummyAssessmentData");
-
-const getAssessmentData = jest
-  .fn()
-  .mockReturnValue(Promise.resolve(dummyAssessmentData));
-
-exports.getAssessmentData = getAssessmentData;
